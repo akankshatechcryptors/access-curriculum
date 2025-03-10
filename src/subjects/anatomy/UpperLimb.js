6f1addfcd4d85a478486de663c76ccd3b9564cb3
sanskartcis <sanskarp@techcryptors.com>
2025-03-10 10:12:41 +0530

    Upper Limb Anatomy

--- src/subjects/anatomy/UpperLimb.js
+++ src/subjects/anatomy/UpperLimb.js
@@ -62,11 +62,17 @@ export const competencies = [
       ],
       'Multimedia': [
         { title: "Resource 1", link: "https://fadavispt.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=13545787" },
-        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17566152" },
-        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=18859210" },
+        { title: "Resource 2", link: "https://fadavispt.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=13460344" },
+        { title: "Resource 3", link: "https://fadavispt.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=13460350" },
+        { title: "Resource 4", link: "https://fadavispt.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=13545795" },
+        { title: "Resource 5", link: "https://accessphysiotherapy.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=19053825" },
+        { title: "Resource 6", link: "https://fadavisat.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17652548" },
+        { title: "Resource 7", link: "https://fadavisat.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17652788" },
+        { title: "Resource 8", link: "https://fadavisat.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17652790" },
       ],
       'Cases': [
-        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219173&gbosContainerID=62&viewByNumber=false&groupid=0#104569796" }
+        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219173&gbosContainerID=62&viewByNumber=false&groupid=0#104569796" },
+        { title: "Resource 2", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219171&gbosContainerID=62&viewByNumber=false&groupid=0#104569619" },
       ],
       'Q & A': [
         { title: "Resource 1", link:"https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38702&resourceid=2479"},
@@ -75,4 +81,107 @@ export const competencies = [
         { title: "Resource 1", link:"https://anatomy.mheducation.com/html/apr.html?animal=human&login=1689270909398"},
       ],
     },    
+    '8.3': {
+      'Textbooks': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/content.aspx?sectionid=202021409&bookid=2478#202021414" },
+        { title: "Resource 2", link: "https://fadavisat.mhmedical.com/content.aspx?sectionid=162870741&bookid=2148#162870755"},
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/content.aspx?sectionid=286274769&bookid=3473#286274782" },
+      ],
+      'Dissection': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/content.aspx?sectionid=238968077&bookid=2803#1174511758" }
+      ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/ViewLarge.aspx?figid=202021413&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021409&multimediaId=undefined" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=286274785&gbosContainerID=0&gbosid=0&groupID=0&sectionId=286274769&multimediaId=undefined" },
+      ],
+      'Multimedia': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17566151" },
+        { title: "Resource 2", link: "https://accessanesthesiology.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=5480279" },
+        { title: "Resource 3", link: "https://accessanesthesiology.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=5480280" },
+        { title: "Resource 4", link: "https://accesssurgery.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=18054256" },
+        { title: "Resource 5", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=18988539" },
+      ],
+      'Cases': [
+        { title: "Resource 1", link: "https://accesssurgery.mhmedical.com/CaseContent.aspx?gbosID=218929&gbosContainerID=84&viewByNumber=false&groupid=1431#105336788" },
+      ],
+      'Q & A': [
+        { title: "Resource 1", link:"https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38702&resourceid=2479"},
+      ],
+      'Anatomy and Physiology': [
+        { title: "Resource 1", link:"https://anatomy.mheducation.com/html/human/paris.html?animal=human&login=1689270909398"},
+      ],
+    },    
+    '8.4': {
+      'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2478&sectionid=202021409#1158277935" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2478&sectionid=202021409#1158277935"},
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2478&sectionid=202021544#202021551" },
+        { title: "Resource 4", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=3473&sectionid=286275045#1207501544" },
+        { title: "Resource 5", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=3473&sectionid=286275133#1207501631" },
+        { title: "Resource 6", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=3473&sectionid=286275178" },
+      ],
+      'Dissection': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/content.aspx?bookid=2803&sectionid=238968077#1174511758" }
+      ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021489&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021472&multimediaId=undefined" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021490&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021472" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021493&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021472&multimediaId=undefined" },
+        { title: "Resource 4", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021494&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021472" },
+        { title: "Resource 5", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021554&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021544" },
+      ],
+      'Multimedia': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17566151" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=18859210" },
+      ],
+      'Q & A': [
+        { title: "Resource 1", link:"https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38702&resourceid=2479"},
+      ],
+      'Anatomy and Physiology': [
+        { title: "Resource 1", link:"https://anatomy.mheducation.com/html/human/paris.html?animal=human&login=1689270909398"},
+      ],
+    }, 
+    '8.5': {
+      'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2478&sectionid=202021641#202021654" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2478&sectionid=202021641#1158278170"},
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=3473&sectionid=286275178#1207501676" },
+        { title: "Resource 4", link: "https://accessmedicine.mhmedical.com/content.aspx?sectionid=286274963&bookid=3473#286274979" },
+      ],
+      'Dissection': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/content.aspx?bookid=2803&sectionid=238968077#1174511767" }
+      ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021647&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021641&multimediaId=undefined" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=202021713&gbosContainerID=0&gbosid=0&groupID=0&sectionId=202021641&multimediaId=undefined" },
+      ],
+      'Multimedia': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17566151" },
+        { title: "Resource 2", link: "https://fadavisat.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17652548" },
+      ],
+      'Q & A': [
+        { title: "Resource 1", link:"https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38702&resourceid=2479"},
+      ],
+      'Anatomy and Physiology': [
+        { title: "Resource 1", link:"https://anatomy.mheducation.com/html/human/paris.html?animal=human&login=1689270909398"},
+      ],
+    }, 
+    '8.6': {
+      'Textbooks': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/content.aspx?bookid=3075&sectionid=255923450#255923465" },
+      ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessphysiotherapy.mhmedical.com/ViewLarge.aspx?figid=255923468&gbosContainerID=0&gbosid=0&groupID=0&sectionId=255923450&multimediaId=undefined" },
+      ],
+      'Multimedia': [
+        { title: "Resource 1", link: "https://accesssurgery.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=18054319" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/updatesContent.aspx?gbosid=569817" },
+      ],
+      'Q & A': [
+        { title: "Resource 1", link:"https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38702&resourceid=2479"},
+      ],
+      'Anatomy and Physiology': [
+        { title: "Resource 1", link:"https://anatomy.mheducation.com/html/human/paris.html?animal=human&login=1689270909398"},
+      ],
+    }, 
   };
