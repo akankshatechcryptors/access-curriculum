8e5a5129157b8523265015bb6ab4423a73858620
akankshatectcryptors <akankshai@techcryptors.com>
2025-03-11 09:25:39 +0530

    Pathology Book Completed with links

---
+++ src/subjects/pathology/NeoplasticDisorders.js
@@ -0,0 +1,133 @@
+export const competencies = [
+    '7.1  Define and classify neoplasia. Describe the characteristics of neoplasia including gross, microscopy, biologic, behaviour and spread. Differentiate between benign from maignant neoplams',
+    '7.2 Describe the molecular basis of cancer',
+    '7.3  Enumerate carcinogens and describe the process of carcinogenesis',
+    '7.4 Describe the effects of tumor on the host including paraneoplastic syndrome',
+    '7.5 Describe immunology and the immune response to cancer',
+
+  ];
+  
+  export const content = {
+    '7.1': {
+      'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=499&sectionid=41568287#57050939" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/content.aspx?sectionid=198220365&bookid=2468&Resultclick=2#1156656061" },
+    ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=41568692&gbosContainerID=0&gbosid=0&groupID=0&sectionId=41568287&multimediaId=undefined" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=41568693&gbosContainerID=0&gbosid=0&groupID=0&sectionId=41568287&multimediaId=undefined" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=198220597&gbosContainerID=0&gbosid=0&groupID=0&sectionId=198220365" },
+        { title: "Resource 4", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=198220598&gbosContainerID=0&gbosid=0&groupID=0&sectionId=198220365" },
+    ],
+      'Flashcards': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630068&categoryId=47417" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630127&categoryId=47416" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630038&categoryId=47418" },
+    ],
+    'Cases':[
+        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219412&gbosContainerID=66&viewByNumber=false&groupid=0#105343990"},
+        { title: "Resource 2", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219371&gbosContainerID=66&viewByNumber=false&groupid=0#105340322"}
+    ],
+      'Q & A': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=604" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=2774" },
+    ],
+    },
+    // Repeat for 1.2 to 1.13 with placeholders
+    '7.2': { 
+         'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2748&sectionid=230839307#1173767224" },
+        { title: "Resource 2", link: "https://accesssurgery.mhmedical.com/content.aspx?bookid=2209&sectionid=168937012#1145755394" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/content.aspx?sectionid=198220365&bookid=2468&Resultclick=2#1156656062"}
+        
+    ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accesssurgery.mhmedical.com/ViewLarge.aspx?figid=169279129&gbosContainerID=0&gbosid=0&groupID=0&sectionId=168937012" },
+        { title: "Resource 2", link: "https://accesssurgery.mhmedical.com/ViewLarge.aspx?figid=169279087&gbosContainerID=0&gbosid=0&groupID=0&sectionId=168937012&multimediaId=undefined" },
+    ],
+      'Flashcards': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630068&categoryId=47417" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630127&categoryId=47416" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630038&categoryId=47418"}
+    ],
+    'Cases':[
+        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219412&gbosContainerID=66&viewByNumber=false&groupid=0#105343990"},
+        { title: "Resource 2", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219371&gbosContainerID=66&viewByNumber=false&groupid=0#105340322"}
+    ],
+      'Q & A': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=604" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=2774" },
+    ],
+    },
+    '7.3': { 
+         'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=499&sectionid=41568287#57051061" },
+        { title: "Resource 2", link: "https://accessbiomedicalscience.mhmedical.com/content.aspx?sectionid=253777560&bookid=3022&Resultclick=2#1179323527" },
+        { title: "Resource 3", link: "https://accessbiomedicalscience.mhmedical.com/content.aspx?sectionid=252313094&bookid=3000#1178013616"}
+    ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessbiomedicalscience.mhmedical.com/ViewLarge.aspx?figid=252313106&gbosContainerID=0&gbosid=0&groupID=0&sectionId=252313094&multimediaId=undefined" },
+        { title: "Resource 2", link: "https://accessbiomedicalscience.mhmedical.com/ViewLarge.aspx?figid=252313107&gbosContainerID=0&gbosid=0&groupID=0&sectionId=252313094" },
+    ],
+      'Flashcards': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630068&categoryId=47417" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630127&categoryId=47416" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630038&categoryId=47418"}
+    ],
+    'Cases':[
+        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219412&gbosContainerID=66&viewByNumber=false&groupid=0#105343990"},
+        { title: "Resource 2", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219371&gbosContainerID=66&viewByNumber=false&groupid=0#105340322"}
+    ],
+      'Q & A': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=604" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=2774" },
+    ],
+    },
+    '7.4': { 
+         'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=499&sectionid=41568287#57051049" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/content.aspx?sectionid=198220365&bookid=2468&Resultclick=2#1156656068" },
+        { title: "Resource 3", link: "https://accessbiomedicalscience.mhmedical.com/content.aspx?sectionid=253779635&bookid=3022#1179325676" }
+    ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=198220597&gbosContainerID=0&gbosid=0&groupID=0&sectionId=198220365" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=198220598&gbosContainerID=0&gbosid=0&groupID=0&sectionId=198220365" },
+    ],
+      'Flashcards': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630068&categoryId=47417" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630127&categoryId=47416" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630038&categoryId=47418"},
+    ],
+    'Cases':[
+        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219412&gbosContainerID=66&viewByNumber=false&groupid=0#105343990"},
+        { title: "Resource 2", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219371&gbosContainerID=66&viewByNumber=false&groupid=0#105340322"}
+    ],
+      'Q & A': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=33710&resourceid=604" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=2774" },
+    ],  
+    },
+    '7.5': { 
+         'Textbooks': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=2629&sectionid=217769996#1163278733" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/content.aspx?bookid=3428&sectionid=284628907#1206189756" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/content.aspx?sectionid=230840668&bookid=2748#241283737"}
+    ],
+      'Images & Tables': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/ViewLarge.aspx?figid=41568778&gbosContainerID=0&gbosid=0&groupID=0&sectionId=41568287" },
+    ],
+      'Flashcards': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630068&categoryId=47417" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630127&categoryId=47416" },
+        { title: "Resource 3", link: "https://accessmedicine.mhmedical.com/flashcard.aspx?gbosContainerId=315&gbosId=630038&categoryId=47418"}
+    ],
+    'Cases':[
+        { title: "Resource 1", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219412&gbosContainerID=66&viewByNumber=false&groupid=0#105343990"},
+        { title: "Resource 2", link: "https://casefiles.mhmedical.com/CaseContent.aspx?gbosID=219371&gbosContainerID=66&viewByNumber=false&groupid=0#105340322"}
+    ],
+      'Q & A': [
+        { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=604" },
+        { title: "Resource 2", link: "https://accessmedicine.mhmedical.com/qa.aspx?groupid=962&categoryid=38710&resourceid=2774" },
+    ],
+    },
+  }
