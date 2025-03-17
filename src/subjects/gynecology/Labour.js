e8cca5c93d60d2fc2f9dbdd49e633c55d0a3b821
akankshatechcryptors <akankshai@techcryptors.com>
2025-03-17 11:34:25 +0530

    Labour subject links done

--- src/subjects/gynecology/Labour.js
+++ src/subjects/gynecology/Labour.js
@@ -1,179 +1,237 @@
 export const competencies = [
-    '28.1   Describe and discuss the common causes, pathogenesis, clinical features, differential diagnosis; investigations; principles of management of infertility – methods of tubal patency, ovulation induction, assisted reproductive techniques',
-    '28.2 Enumerate the assessment and restoration of tubal latency',
-    '28.3  Describe the principles of ovulation induction',
-    '28.4 Enumerate the various Assisted Reproduction Techniques',
+    '13.1   Enumerate and discuss the physiology of normal labor, mechanism of labor in occipito-anterior presentation; monitoring of labor including partogram; conduct of labor, pain relief; principles of induction and acceleration of labor; management of third stage of labor',
+    '13.2   Define, describe the causes, pathophysiology, diagnosis, investigations and management of preterm labor, PROM and postdated pregnancy',
+    '13.3   Observe/ assist in the performance of an artificial rupture of membranes',
+    '13.4   Demonstrate the stages of normal labor in a simulated environment / mannequin and counsel on methods of safe abortion.',
+    '13.5   Observe and assist the conduct of a normal vaginal delivery'
 
   ];
   
   export const content = {
-    '28.1': {
+    '13.1': {
       'Textbook Chapters': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/content.aspx?bookid=2977&sectionid=263817742" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/content.aspx?bookid=2511&sectionid=206168939#1159982949" },
     ],
       'Quick References': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-        { title: "Resource 4", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=569472" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569452" },
+        { title: "Resource 3", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569475" },
     ],
-      'Differential Diagnosis': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-    ],
+
     'Clinical Cases':[
-        { title: "Resource 1", link: ""},
-        { title: "Resource 2", link: ""}
+        { title: "Resource 1", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284303&gbosContainerID=131#142565107"},
+        { title: "Resource 2", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568949&gbosContainerID=271#259868066"},
+        { title: "Resource 3", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284300&gbosContainerID=131#142564869"},
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284301&gbosContainerID=131"},
+        { title: "Resource 5", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568933&gbosContainerID=271#259865833"},
+        { title: "Resource 6", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=531499&gbosContainerID=231#248082674"},
+        { title: "Resource 7", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284296&gbosContainerID=131"},
     ],
       'Patient Education': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
     ],
     'Boards & Beyond': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 2", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 3", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 4", link: "https://www.boardsbeyond.com/login" },
     ],
     'Multimedia:Podcast': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=19277609" },
     ],
     'Review Questions': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=3030" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 3", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
     ],
     '2 Minute Medicine/2MM':[
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=626981&sectionId=0&Resultclick=2" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=634757&sectionId=0&Resultclick=2" },
     ]
     },
     // Repeat for 1.2 to 1.13 with placeholders
-    '28.2': { 
+    '13.2': { 
          'Textbook Chapters': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/content.aspx?bookid=2511&sectionid=206169848" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/content.aspx?bookid=2977&sectionid=263821201" },
+        { title: "Resource 3", link: "https://obgyn.mhmedical.com/content.aspx?bookid=3238&sectionid=269820655" }
     ],
       'Quick References': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-        { title: "Resource 4", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=569472" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569452" },
+        { title: "Resource 3", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569475" },
     ],
-      'Differential Diagnosis': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-    ],
+
     'Clinical Cases':[
-        { title: "Resource 1", link: ""},
-        { title: "Resource 2", link: ""}
+        { title: "Resource 1", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284303&gbosContainerID=131#142565107"},
+        { title: "Resource 2", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568949&gbosContainerID=271#259868066"},
+        { title: "Resource 3", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284300&gbosContainerID=131#142564869"},
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284301&gbosContainerID=131"},
+        { title: "Resource 5", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568933&gbosContainerID=271#259865833"},
+        { title: "Resource 6", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=531499&gbosContainerID=231#248082674"},
+        { title: "Resource 7", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284296&gbosContainerID=131"},
     ],
       'Patient Education': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
     ],
     'Boards & Beyond': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 2", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 3", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 4", link: "https://www.boardsbeyond.com/login" },
     ],
     'Multimedia:Podcast': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=19277609" },
     ],
     'Review Questions': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=3030" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 3", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
     ],
     '2 Minute Medicine/2MM':[
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=626981&sectionId=0&Resultclick=2" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=634757&sectionId=0&Resultclick=2" },
     ]
         
     },
-    '28.3': { 
+    '13.3': { 
          'Textbook Chapters': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/content.aspx?sectionid=257227192&bookid=2977#263818670" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/content.aspx?sectionid=269576916&bookid=3238#269818008" },
     ],
       'Quick References': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-        { title: "Resource 4", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=569472" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569452" },
+        { title: "Resource 3", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569475" },
     ],
-      'Differential Diagnosis': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-    ],
+
     'Clinical Cases':[
-        { title: "Resource 1", link: ""},
-        { title: "Resource 2", link: ""}
+        { title: "Resource 1", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284303&gbosContainerID=131#142565107"},
+        { title: "Resource 2", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568949&gbosContainerID=271#259868066"},
+        { title: "Resource 3", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284300&gbosContainerID=131#142564869"},
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284301&gbosContainerID=131"},
+        { title: "Resource 5", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568933&gbosContainerID=271#259865833"},
+        { title: "Resource 6", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=531499&gbosContainerID=231#248082674"},
+        { title: "Resource 7", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284296&gbosContainerID=131"},
     ],
       'Patient Education': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
     ],
     'Boards & Beyond': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 2", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 3", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 4", link: "https://www.boardsbeyond.com/login" },
     ],
     'Multimedia:Podcast': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=19277609" },
     ],
     'Review Questions': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=3030" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 3", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
     ],
     '2 Minute Medicine/2MM':[
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=626981&sectionId=0&Resultclick=2" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=634757&sectionId=0&Resultclick=2" },
     ]
         
     },
-    '28.4': { 
+    '13.4': { 
          'Textbook Chapters': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/content.aspx?sectionid=206968141&bookid=2559#206968241" },
     ],
       'Quick References': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-        { title: "Resource 4", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=569472" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569452" },
+        { title: "Resource 3", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569475" },
     ],
-      'Differential Diagnosis': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
-        { title: "Resource 3", link: "" },
-    ],
+
     'Clinical Cases':[
-        { title: "Resource 1", link: ""},
-        { title: "Resource 2", link: ""}
+        { title: "Resource 1", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284303&gbosContainerID=131#142565107"},
+        { title: "Resource 2", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568949&gbosContainerID=271#259868066"},
+        { title: "Resource 3", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284300&gbosContainerID=131#142564869"},
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284301&gbosContainerID=131"},
+        { title: "Resource 5", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568933&gbosContainerID=271#259865833"},
+        { title: "Resource 6", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=531499&gbosContainerID=231#248082674"},
+        { title: "Resource 7", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284296&gbosContainerID=131"},
     ],
       'Patient Education': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
     ],
     'Boards & Beyond': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 2", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 3", link: "https://www.boardsbeyond.com/login" },
+        { title: "Resource 4", link: "https://www.boardsbeyond.com/login" },
     ],
     'Multimedia:Podcast': [
         { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=19277609" },
     ],
     'Review Questions': [
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=3030" },
+        { title: "Resource 2", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 3", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+        { title: "Resource 4", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
     ],
     '2 Minute Medicine/2MM':[
-        { title: "Resource 1", link: "" },
-        { title: "Resource 2", link: "" },
+        { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=626981&sectionId=0&Resultclick=2" },
+        { title: "Resource 2", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=634757&sectionId=0&Resultclick=2" },
     ]
         
     },
+    '13.5': { 
+      'Textbook Chapters': [
+     { title: "Resource 1", link: "https://accessmedicine.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=17964594" },
+ ],
+   'Quick References': [
+     { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=569472" },
+     { title: "Resource 2", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569452" },
+     { title: "Resource 3", link: "https://access.mhmedical.com/updatescontent.aspx?gbosid=569475" },
+ ],
+
+ 'Clinical Cases':[
+     { title: "Resource 1", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284303&gbosContainerID=131#142565107"},
+     { title: "Resource 2", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568949&gbosContainerID=271#259868066"},
+     { title: "Resource 3", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=284300&gbosContainerID=131#142564869"},
+     { title: "Resource 4", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284301&gbosContainerID=131"},
+     { title: "Resource 5", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=568933&gbosContainerID=271#259865833"},
+     { title: "Resource 6", link: "https://access.mhmedical.com/CaseContent.aspx?gbosID=531499&gbosContainerID=231#248082674"},
+     { title: "Resource 7", link: "https://obgyn.mhmedical.com/CaseContent.aspx?gbosID=284296&gbosContainerID=131"},
+ ],
+   'Patient Education': [
+     { title: "Resource 1", link: "" },
+ ],
+ 'Boards & Beyond': [
+     { title: "Resource 1", link: "https://www.boardsbeyond.com/login" },
+     { title: "Resource 2", link: "https://www.boardsbeyond.com/login" },
+     { title: "Resource 3", link: "https://www.boardsbeyond.com/login" },
+     { title: "Resource 4", link: "https://www.boardsbeyond.com/login" },
+ ],
+ 'Multimedia:Podcast': [
+     { title: "Resource 1", link: "" },
+     { title: "Resource 2", link: "https://obgyn.mhmedical.com/MultimediaPlayer.aspx?MultimediaID=19277609" },
+ ],
+ 'Review Questions': [
+     { title: "Resource 1", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=3030" },
+     { title: "Resource 2", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+     { title: "Resource 3", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+     { title: "Resource 4", link: "https://obgyn.mhmedical.com/qa.aspx?groupid=964&categoryid=0&resourceid=2501" },
+ ],
+ '2 Minute Medicine/2MM':[
+     { title: "Resource 1", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=626981&sectionId=0&Resultclick=2" },
+     { title: "Resource 2", link: "https://access.mhmedical.com/updatesContent.aspx?gbosid=634757&sectionId=0&Resultclick=2" },
+ ]
+     
+ },
+ 
    
   }
